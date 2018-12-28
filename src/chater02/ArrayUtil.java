b9fcc01e90e8ef984dc445cbe64d6f4d2d955160
choi <choi@192.168.0.70>
2018-12-28 14:58:21 +0900

    test

---
+++ src/chater02/ArrayUtil.java
@@ -0,0 +1,19 @@
+package chater02;
+
+public class ArrayUtil {
+
+	static double [] intToDouble( int[] source ){
+		return null;
+		
+	}
+
+	static int [] doubleToInt( double[] source ){
+		return null;
+		
+	}
+	static int [] concat( int[] s1, int[] s2 ){
+		return s2;
+		
+	}
+
+}
---
+++ src/chater02/UtilTest.java
@@ -0,0 +1,30 @@
+package chater02;
+
+
+public class UtilTest {
+
+	public static void main(String[] args) {
+		int[] a1={10,11,12,13};
+		double[]d1 =ArrayUtil.intToDouble(a1);
+		for(double dd:d1)
+		{
+			System.out.println(dd);
+		}
+			
+		double[] d2={10.1,11.1,12.2,13.3};
+		int[] a2=ArrayUtil.doubleToInt(d2);
+		for(int aa:a2)
+		{
+			System.out.println(aa);
+		}
+		
+		int [] a3= ArrayUtil.concat(a1, a2);
+		for(int a:a3)
+		{
+			System.out.println(a);
+		}
+		
+		
+		
+	}
+}
